bf4f81f0b96c0747640b6600d0b4a308b2003a8c
Jan <jk@smartsupp.com>
2020-11-01 01:48:29 +0100

    reenable cors

--- src/http/middleware/cors.ts
+++ src/http/middleware/cors.ts
@@ -4,10 +4,10 @@ export default () => {
 
 	return async (ctx: Context, next) => {
 		ctx.set('Access-Control-Allow-Origin', '*')
-		//ctx.set('Access-Control-Allow-Credentials', 'true')
-		//ctx.set('Access-Control-Allow-Methods', 'POST, OPTIONS, GET, PATCH')
-		//ctx.set('Access-Control-Max-Age', '1728000')
-		//ctx.set('Access-Control-Allow-Headers', 'X-Request, X-Requested-With, Content-Type, Cookie, Authorization')
+		ctx.set('Access-Control-Allow-Credentials', 'true')
+		ctx.set('Access-Control-Allow-Methods', 'POST, OPTIONS, GET, PATCH')
+		ctx.set('Access-Control-Max-Age', '1728000')
+		ctx.set('Access-Control-Allow-Headers', 'X-Request, X-Requested-With, Content-Type, Cookie, Authorization')
 		await next()
 	}
 
