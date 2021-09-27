a370c6da37079ab816c22d7869b3c8573fee0821
Jan <jan.kasztura@seznam.cz>
2021-09-27 20:48:18 +0200

    modify status endpoint

--- src/http/routers/api/main.ts
+++ src/http/routers/api/main.ts
@@ -1,20 +1,21 @@
 
 import Router from "koa-router";
+import { Sequelize } from "sequelize";
 import { Context } from "../../types";
+import HttpStatus from 'http-status-codes'
 
 /**
  * @injectable(http.router.main)
+ * @param sequelize @inject(database.client)
  */
-export function createMainRouter(): Router {
+export function createMainRouter(sequelize: Sequelize): Router {
 	const router = new Router()
 
 	router.get('/status', async (ctx: Context) => {
-		let dbStatus: string
-		try {
-			dbStatus = 'Database connected successfuly'
-		} catch (err) {
-			dbStatus = err.message
-		}
+		const dbStatus: number = await check(
+			() => sequelize.authenticate(),
+			(err) => console.error(err)
+		)
 		ctx.send({ ok: true, db: dbStatus }, 200)
 	})
 
@@ -23,4 +24,23 @@ export function createMainRouter(): Router {
 	})
 
 	return router
+}
+
+async function check(callback: () => any, onError?: (err: Error) => void, timeout = 5000): Promise<number> {
+	const promise = new Promise<void>((resolve, reject) => {
+		callback().then(resolve).catch(reject)
+
+		setTimeout(() => {
+			reject(new Error('Connection timeout'))
+		}, timeout)
+	})
+	try {
+		await promise
+		return HttpStatus.OK
+	} catch (err) {
+		if (onError) {
+			onError(err)
+		}
+		return HttpStatus.SERVICE_UNAVAILABLE
+	}
 }
